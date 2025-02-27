06e60dd4f03bcc1abbbb533e1d006cb5e2e8a101
Tamara Mwanzali Kaka <billnyetherussianspy679@gmail.com>
2025-02-27 13:47:57 +0300

    saving changes

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,30 @@
 from models import Dog
+from sqlalchemy import create_engine
 
-def create_table(base):
-    pass
+def create_table(base,engine):
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
+
 
 def get_all(session):
-    pass
+    return session.query(Dog)
+    
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter_by(name=name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter_by(id=id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    db_dog = session.query(Dog).filter(Dog.id == dog.id).first()
+    
+    if db_dog:
+        db_dog.breed = breed  
+        session.commit()  
